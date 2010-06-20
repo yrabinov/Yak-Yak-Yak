1d41e76424b46ac610dca0e1a62764a6b763700e
Yakov Rabinovich <yakov.rabinovich@gmail.com>
2010-06-20 17:36:04 -0400

    -m Initial commit

---
+++ config/initializers/rspec_generator.rb
@@ -0,0 +1,6 @@
+Yak::Application.configure do
+  config.generators do |g|
+    g.integration_tool :rspec
+    g.test_framework   :rspec
+  end
+end if defined? Yak::Application
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'e5d89a039cb20d8438ccc96e9375af130b0be8310652da987ae2bd9468c85c861684adfdcc56d4914b8096fd42c71eeb847ee5230d81a3c4adbb463e9a7326de'
